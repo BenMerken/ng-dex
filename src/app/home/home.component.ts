14f5c02e74805969835c31a579d79d92a843d198
Ben Merken <ben.merken@gmail.com>
2024-07-31 13:08:13 +0200

    Add custom theme

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
+import {MatCheckboxModule} from '@angular/material/checkbox'
 
 @Component({
 	selector: 'dex-home',
 	standalone: true,
-	imports: [],
+	imports: [MatCheckboxModule],
 	templateUrl: './home.component.html',
 	styleUrl: './home.component.scss'
 })
